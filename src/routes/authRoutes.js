ac1d94bc60ef604d0ff18fb88c8d71b3b4619da7
Musab Zahid Ansari <musabzahida@gmail.com>
2025-06-09 16:47:26 +0530

    moved getCurrentUser code to authController

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-const {register,login} = require('../controllers/authController');
+const {register,login, getCurrentUser} = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/me', (req, res) => {
-    const authHeader = req.headers.authorization;
-  
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(401).json({ user: null, message: 'No token provided' });
-    }
-  
-    const token = authHeader.split(' ')[1];
-
-    console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
-  
-    // try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log('decoded', decoded);
-      return res.json({ user: decoded }); // optionally only send user id/email
-    // } catch (err) {
-    //   return res.status(401).json({ user: null, message: 'Invalid token' });
-    // }
-  });
+router.get('/me', getCurrentUser);
 
 
 module.exports = router;
